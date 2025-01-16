d361ca622d374f5fa4373e289d4dc081b9733879
yaoxingpu <yaoxpu@163.com>
2025-01-16 17:58:48 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let mut price = 2;
+    if quantity > 40 {
+        price = 1;
+    }
+    price * quantity
+}
 
 // Don't modify this function!
 #[test]
